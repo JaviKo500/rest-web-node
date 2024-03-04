28f44738e3cf0fe1263b4e8efcf469566dc65f10
JaViKo500 <javikogutierrez64@gmail.com>
2024-03-03 20:49:21 -0500

    add public app test

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 export class Server {
    
    private app = express();
@@ -7,7 +8,15 @@ export class Server {
 
       // * middlewares
       // * public folders
-      this.app.use( express.static('public') )
+      this.app.use( express.static('public') );
+
+      this.app.get( '*', ( req, res ) => {
+         const indexPath = path.join( __dirname, '../../public/index.html' );
+         console.log('<--------------- JK Server --------------->');
+         console.log(req.url);
+         res.sendFile(indexPath)
+         return;
+      });
       this.app.listen( 3000, () => {
          console.log('<--------------- JK Server --------------->');
          console.log(`server running port ${3000}`);

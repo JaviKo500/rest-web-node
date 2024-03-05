01ce04d3be6a599c0aa5880c5b204f8ee8bd9d8d
JaViKo500 <javikogutierrez64@gmail.com>
2024-03-04 22:10:00 -0500

    port and path

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/envs";
 import { Server } from "./presentation/server";
 
 ( async () => {
@@ -7,6 +8,6 @@ import { Server } from "./presentation/server";
 async function main() {
    console.log('<--------------- JK App --------------->');
    console.log('main');
-   const server = new Server();
+   const server = new Server( { port: envs?.PORT ?? 3000, public_path: envs.PUBLIC_PATH } );
    await server.start();
 }

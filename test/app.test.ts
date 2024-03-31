2537bf9f5ef5ebe0c8beeb60c0f0831e86de2d3c
JaViKo500 <javikogutierrez64@gmail.com>
2024-03-31 15:29:22 -0500

    test in app

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,16 @@
+import { envs } from '../src/config/envs';
+import { Server } from '../src/presentation/server';
+jest.mock('../src/presentation/server.ts');
 describe('App.test', () => {
-   test( 'should work', () => {
-      expect( true ).toBeTruthy();
+   test( 'should call server with arguments and start', async () => {
+      await import('../src/app');
+      expect( Server ).toHaveBeenCalledTimes(1);
+      expect( Server ).toHaveBeenCalledWith({
+         port: envs.PORT,
+         public_path: envs.PUBLIC_PATH,
+         routes: expect.any(Function),
+      });
+
+      expect( Server.prototype.start ).toHaveBeenCalledWith()
    });
 });
